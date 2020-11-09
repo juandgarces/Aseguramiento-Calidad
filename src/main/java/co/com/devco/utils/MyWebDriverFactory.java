16cdffa1c239f980ac3719d2180ff529098d5fd8
Juan David Garces <jdgr1987@gmail.com>
2020-11-09 17:34:51 -0500

    Agregar clase MyWebDriver

---
+++ src/main/java/co/com/devco/utils/MyWebDriverFactory.java
@@ -0,0 +1,69 @@
+package co.com.devco.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static co.com.devco.utils.Constantes.*;
+
+
+public class MyWebDriverFactory {
+
+    private static WebDriver driver;
+
+    public static MyWebDriverFactory web() {
+        driver = getDriver();
+        driver.manage().window().maximize();
+        return new MyWebDriverFactory();
+    }
+
+    public static WebDriver getDriverBrowser(){
+        return driver;
+    }
+
+    private static WebDriver getDriver() {
+        String webdriverstr = DRIVER;
+
+        if(webdriverstr == null) {
+            webdriverstr="chrome";
+        }
+
+        switch(webdriverstr) {
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+                FirefoxOptions ffOptions = new FirefoxOptions();
+                ffOptions.addArguments("--ignore-certificate-errors");
+
+                return new FirefoxDriver(ffOptions);
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "C:/Webdriver/chromedriver.exe");
+                return new ChromeDriver();
+
+            case "ie":
+                System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+
+                DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+                capabilities.setCapability("ignoreZoomSetting", true);
+                capabilities.setCapability("ignoreProtectedModeSettings", true);
+                capabilities.setCapability("requireWindowFocus", true);
+                capabilities.setCapability("enableNativeEvents", true);
+                capabilities.setCapability("enablePersistentHover", true);
+                capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+                capabilities.setCapability("initialBrowserUrl", "http://");
+                return new InternetExplorerDriver(capabilities);
+
+            default:
+                throw new RuntimeException("Unsupported webdriver: " + webdriverstr);
+        }
+    }
+
+    public WebDriver onTheWebsite(String url) {
+        driver.get(url);
+        return driver;
+    }
+
+}
--- src/test/java/co/com/devco/stepdefinitions/PetbookStepDefinitions.java
+++ src/test/java/co/com/devco/stepdefinitions/PetbookStepDefinitions.java
@@ -5,14 +5,17 @@ import co.com.devco.questions.ValidarImagenesPerro;
 import co.com.devco.questions.ValidarPagina;
 import co.com.devco.tasks.Ingresar;
 import co.com.devco.tasks.SeleccionarOpcion;
+import co.com.devco.utils.MyWebDriverFactory;
 import cucumber.api.java.Before;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
 import net.serenitybdd.screenplay.Consequence;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
+import static co.com.devco.userinterfaces.PetbookHomePage.PETBOOK_HOME_PAGE;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
@@ -28,9 +31,8 @@ public class PetbookStepDefinitions {
 
     @Dado("^Que Juan ingresa a la pagina Pet Book$")
     public void queJuanIngresaALaPaginaPetBook() {
-        theActorInTheSpotlight().attemptsTo(
-                Ingresar.url()
-        );
+        theActorInTheSpotlight().whoCan(BrowseTheWeb.with(
+                MyWebDriverFactory.web().onTheWebsite(PETBOOK_HOME_PAGE)));
     }
 
 
